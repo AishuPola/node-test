58aaaef4e5aa172df59736af69a1e682dff8d1f3
AishuPola <aishwarya.pola@proclink.com>
2024-08-13 01:02:48 +0530

    orders

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -5,7 +5,7 @@ import { getProductById } from "../services/cart.service.js";
 async function placeOrderCtr(req, res) {
   const data = req.body;
   const userId = data.userId;
-  const cartData = await getCartByUserId(userId);
+  const cartData = await getProductById(userId);
 
   if (
     !cartData.data ||
